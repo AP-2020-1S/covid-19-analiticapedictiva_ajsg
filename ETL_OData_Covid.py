0181686d80a3a95f3cd8c1b2a94e370c3d16304a
andrescadavid <andrescadavid@lahaus.com>
2020-09-06 11:29:39 -0500

    Update ETL_OData_Covid.py

--- ETL_OData_Covid.py
+++ ETL_OData_Covid.py
@@ -17,9 +17,8 @@ from scipy.integrate import odeint
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
-
-# model = ARIMA(test['casos_diff'], order=(5,1,0))
 # %%
+
 
 def extract_data(api_url, limit):
     # Funcion para la extraccion de los datos via SODA API ODATA
@@ -34,7 +33,7 @@ def extract_data(api_url, limit):
         data_len = len(data)
         if 'error' not in data or len(data) > 0:
             df = pd.DataFrame.from_dict(data, orient='columns')
-            df['extracted_at_utc'] = pd.to_datetime('now',utc=True)
+            df['extracted_at_utc'] = pd.to_datetime('now', utc=True)
             appended_data.append(df)
             offset = offset + limit
     return pd.concat(appended_data, ignore_index=True, sort=False)
@@ -53,6 +52,7 @@ def data_transform(df, start_date):
     df_in['fecha_recuperado'] = pd.to_datetime(df_in['fecha_recuperado'])
     df_in['fecha_diagnostico'] = pd.to_datetime(df_in['fecha_diagnostico'])
     return df_in, df_dates
+
 
 def data_agg_col(df, df_dates, city_filter):
     # Funcion para realizar agregaciones
@@ -76,16 +76,17 @@ def data_agg_col(df, df_dates, city_filter):
     df_full = pd.merge(df_full, df_recuperados, how='left', on=['fecha'])
     return df_full
 
+
 # evaluate an ARIMA model for a given order (p,d,q)
-def evaluate_arima_model(X, arima_order):
-	# prepare training dataset
-    train_size = int(len(X) * 0.8)
+def evaluate_arima_model(X, arima_order, train_perc):
+    # prepare training dataset
+    train_size = int(len(X) * train_perc)
     train, test = X[0:train_size], X[train_size:]
     history = [x for x in train]
     # make predictions
     predictions = list()
     for t in range(len(test)):
-        model = ARIMA(history, order=arima_order)
+        model = ARIMA(history, order=(arima_order))
         model_fit = model.fit(disp=0)
         yhat = model_fit.forecast()[0]
         predictions.append(yhat)
@@ -97,20 +98,20 @@ def evaluate_arima_model(X, arima_order):
 
 # evaluate combinations of p, d and q values for an ARIMA model
 def evaluate_models(dataset, p_values, d_values, q_values):
-	dataset = dataset.astype('float32')
-	best_score, best_cfg = float("inf"), None
-	for p in p_values:
-		for d in d_values:
-			for q in q_values:
-				order = (p,d,q)
-				try:
-					mse = evaluate_arima_model(dataset, order)
-					if mse < best_score:
-						best_score, best_cfg = mse, order
-					print('ARIMA%s MSE=%.3f' % (order,mse))
-				except:
-					continue
-	print('Best ARIMA%s MSE=%.3f' % (best_cfg, best_score))
+    dataset = dataset.astype('float32')
+    best_score, best_cfg = float("inf"), None
+    for p in p_values:
+        for d in d_values:
+            for q in q_values:
+                order = (p,d,q)
+                try:
+                    mse = evaluate_arima_model(dataset, order, 0.8)
+                    if mse < best_score:
+                        best_score, best_cfg = mse, order
+                    print('ARIMA%s MSE=%.3f' % (order,mse))
+                except:
+                    continue
+    print('Best ARIMA%s MSE=%.3f' % (best_cfg, best_score))
 #def data_agg_main_cities(df, main_cities)
 
 '''from pandas import read_csv
@@ -122,7 +123,9 @@ print('ADF Statistic: %f' % result[0])
 print('p-value: %f' % result[1])
 print('Critical Values:')
 for key, value in result[4].items():
-	print('\t%s: %.3f' % (key, value))'''
+    print('\t%s: %.3f' % (key, value))'''
+
+
 # The SIR model differential equations.
 def deriv(y, t, N, beta, gamma):
     S, I, R = y
@@ -132,20 +135,85 @@ def deriv(y, t, N, beta, gamma):
     return dSdt, dIdt, dRdt
 
 
-def data_prep_normalize(df_full):
-    df_full['casos_lag'] = df_full['c_caso'].shift(1)
-    df_full['fallecidos_lag'] = df_full['c_fallecido'].shift(1)
-    df_full['recuperados_lag'] = df_full['c_recuperado'].shift(1)
-    df_full = df_full.replace({0: np.nan})
-    df_full['casos_diff'] = (df_full['casos_lag'] - df_full['c_caso'])/df_full['c_caso']
-    df_full['fallecidos_diff'] = (df_full['fallecidos_lag'] - df_full['c_fallecido']) / df_full['c_fallecido']
-    df_full['recuperados_diff'] = (df_full['recuperados_lag'] - df_full['c_recuperado']) / df_full['c_recuperado']
-    df_full['tasa_casos'] = df_full['casos_lag']/df_full['c_caso']
-    df_full['tasa_fallecitos'] = df_full['fallecidos_lag']/df_full['c_fallecido']
-    df_full['tasa_recuperados'] = df_full['recuperados_lag']/ df_full['c_recuperado']
-    df_full = df_full.fillna(0)
+def data_prep_normalize(df_o):
+    df = df_o.copy()
+    df['casos_lag'] = df['c_caso'].shift(1)
+    df['fallecidos_lag'] = df['c_fallecido'].shift(1)
+    df['recuperados_lag'] = df['c_recuperado'].shift(1)
+    df = df.replace({0: np.nan})
+    df['casos_diff'] = (df['c_caso'] - df['casos_lag'])/df['casos_lag']
+    df['fallecidos_diff'] = (df['c_fallecido'] - df['fallecidos_lag']) / df['fallecidos_lag']
+    df['recuperados_diff'] = (df['c_recuperado'] - df['recuperados_lag']) / df['recuperados_lag']
+    df['tasa_casos'] = df['casos_lag']/df['c_caso']
+    df['tasa_fallecitos'] = df['fallecidos_lag']/df['c_fallecido']
+    df['tasa_recuperados'] = df['recuperados_lag']/ df['c_recuperado']
+
+    # Eliminacion de Outliers
+    quant_95_casos = df['casos_diff'].quantile(.95)
+    mean_casos = df['casos_diff'].mean()
+
+    quant_95_fallecidos = df['fallecidos_diff'].quantile(.95)
+    mean_fallecidos = df['fallecidos_diff'].mean()
+
+    quant_95_recuperados = df['recuperados_diff'].quantile(.95)
+    mean_recuperados = df['recuperados_diff'].mean()
+
+    df['casos_diff'] = df['casos_diff'].apply(lambda x: x if x <= quant_95_casos else mean_casos)
+    df['fallecidos_diff'] = df['fallecidos_diff'].apply(lambda x: x if x <= quant_95_fallecidos else mean_fallecidos)
+    df['recuperados_diff'] = df['recuperados_diff'].apply(lambda x: x if x <= quant_95_recuperados else mean_recuperados)
+
+    df = df.fillna(0)
     # Eliminacion de outliers ultimas fechas por datos faltantes desde la fuente
-    return df_full.drop(df_full.tail(2).index, inplace=True)
+    df.drop(df.tail(2).index, inplace=True)
+    return df
+
+
+def arima_model_imp(df, train_perc, pred_range, ranges_casos, ranges_recu, ranges_fall):
+    df_entrenamiento = df[['fecha', 'casos_diff']].iloc[:int(round((len(df) * train_perc) - 1, 0))]
+    df_entrenamiento_recu = df[['fecha', 'recuperados_diff']].iloc[:int(round((len(df) * train_perc) - 1, 0))]
+    df_entrenamiento_fall = df[['fecha', 'fallecidos_diff']].iloc[:int(round((len(df) * train_perc) - 1, 0))]
+    serie_entrenamienti = df_entrenamiento.set_index('fecha')
+    serie_entrenamienti_recu = df_entrenamiento_recu.set_index('fecha')
+    serie_entrenamienti_fall = df_entrenamiento_fall.set_index('fecha')
+
+    model_casos = ARIMA(serie_entrenamienti['casos_diff'], order=(ranges_casos))
+    model_fit_casos = model_casos.fit(disp=0)
+    model_recu = ARIMA(serie_entrenamienti_recu['recuperados_diff'], order=(ranges_recu))
+    model_fit_recu = model_recu.fit(disp=0)
+    model_fall = ARIMA(serie_entrenamienti_fall['fallecidos_diff'], order=(ranges_fall))
+    model_fit_fall = model_fall.fit(disp=0)
+    # Pronosticos corto plazo
+    yhat_corto_casos = model_fit_casos.forecast(steps=pred_range[0])[0]
+    yhat_corto_recuperados = model_fit_recu.forecast(steps=pred_range[0])[0]
+    yhat_corto_fallecidos = model_fit_fall.forecast(steps=pred_range[0])[0]
+    # Pronosticos largo plazo
+    yhat_largo_casos = model_fit_casos.forecast(steps=pred_range[1])[0]
+    yhat_largo_recuperados = model_fit_recu.forecast(steps=pred_range[1])[0]
+    yhat_largo_fallecidos = model_fit_fall.forecast(steps=pred_range[1])[0]
+    response = [
+        {'pronostico_corto_casos': yhat_corto_casos,
+         'pronostico_largo_casos': yhat_largo_casos},
+        {'pronostico_corto_recuperados': yhat_corto_recuperados,
+         'pronostico_largo_recuperados': yhat_largo_recuperados},
+        {'pronostico_corto_fallecidos': yhat_corto_fallecidos,
+         'pronostico_largo_fallecidos': yhat_largo_fallecidos},
+                ]
+    return response
+
+def error_calculation(df, train_perc,range_c, range_rec, range_fall):
+    serie_casos = df.set_index('fecha')
+    serie_recuperados = df.set_index('fecha')
+    serie_fallecidos = df.set_index('fecha')
+    error_casos = evaluate_arima_model(serie_casos['casos_diff'], range_c, train_perc)
+    error_recu = evaluate_arima_model(serie_recuperados['recuperados_diff'], range_rec, train_perc)
+    error_fall = evaluate_arima_model(serie_fallecidos['fallecidos_diff'], range_fall, train_perc)
+    response = [
+        {'error_casos': error_casos},
+        {'error_recuperados': error_recu},
+        {'error_fallecidos': error_fall}
+
+    ]
+    return response
 
 
 # %%
@@ -153,7 +221,7 @@ def main():
     # %%
     api_url = "https://www.datos.gov.co/resource/gt2j-8ykr.json"
     df_casos = extract_data(api_url, 50000)
-    df_casos, df_dates = data_transform(df_casos,date(2020,3,1))
+    df_casos, df_dates = data_transform(df_casos, date(2020, 3, 1))
     df_full = data_agg_col(df_casos, df_dates, None)
     df_full_medellin = data_agg_col(df_casos, df_dates, ['Medellín'])
     df_full_bogota = data_agg_col(df_casos, df_dates, ['Bogotá D.C.'])
@@ -170,37 +238,44 @@ def main():
     df_norm_barranquilla = data_prep_normalize(df_full_barranquilla)
     # Visualizar la distribucion de los datos graficamente posterior a la normalizacion
     # plt.plot(df_full['fecha'],df_full['casos_diff'])
-    # Crea el preprocesador
-    #scaler = StandardScaler()
-    #x_scl_fit = scaler.fit(df_full['fecha'].array,df_full['c_caso'].array)
-    # Entrena el preprocesador. Note que se calcula
-    # unicamente para el conjunto de entrenamiento
-    #scaler.fit(X_train)
-    # Escala los conjuntos de entrenamiento y prueba
-    #X_train = scaler.transform(X_train)
-    #X_test = scaler.transform(X_test)
-    # Calculo Tasa de contagios - Muertes - Recuperado
     # Implementacion ARIMA
-    df_entrenamiento = df_full[['fecha', 'casos_diff']].iloc[:int(round((len(df_full) * 0.8) - 1, 0))]
-    test = df_entrenamiento.set_index('fecha')
-    #p_v = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-    p_v = [0,3,5]
-    d_v = [0,1]
-    q_v = [0,3,5]
-    #q_v = [0,1,2,3,4,5,6,7,8,9,10]
-    #evaluate_models(test['casos_diff'],p_v,d_v,q_v)
-    error = evaluate_arima_model(test['casos_diff'], [5,0,0])
-    model = ARIMA(test['casos_diff'], order=(5, 0, 0))
-    model_fit = model.fit(disp=0)
-    # Pronostico proximos 7 dias
-    yhat = model_fit.forecast(steps=7)[0]
-    # Pronostico proximos 30 dias
-    yhat = model_fit.forecast(steps=30)[0]
-    #plt.plot(df_full['fecha'],df_full['c_caso'])
+    # Bloque validacion del modelo y deteccion p, d, q optimo (Solo aplicar para re calibrar el modelo)
+    '''p = [0,2,4,6]
+    d = [0,1]
+    q = [0,3,4,6]
+    df_entrenamiento = df_norm_medellin[['fecha', 'fallecidos_diff']].iloc[:int(round((len(df_norm_medellin) * 0.8) - 1, 0))]
+    test = df_norm_cali.set_index('fecha')
+    evaluate_models(test['casos_diff'], p, d, q)'''
+    ###
+    # Colombia casos [0, 0, 2] MSE=0.087
+    # Colombia Recuperados [0, 0, 2] MSE=0.059
+    # Colombia Fallecidos [6, 1, 2] MSE=0.009
+    # Medellin casos [4, 1, 2] MSE=0.065
+    # Medellin recuperados [2, 1, 2] MSE=0.084
+    # Medellin Fallecidos [0, 0, 2] MSE=0.158
+    # Cali casos [6, 1, 0] MSE=0.621
+    # Cali recuperados [2, 1, 4] MSE=0.138
+    # Cali fallecidos [6, 1, 0] MSE=0.068
+    # Barranquilla casos [4, 0, 0] MSE=0.430
+    # Barranquilla recuperados [0, 0, 3] MSE=0.275
+    # Barranquilla Fallecidos [0, 0, 6] MSE=0.940
+    # Cartagena Casos [4, 0, 3] MSE=1.043
+    # Cartagena recuperados [2, 0, 0] MSE=0.327
+    # Cartagena fallecidos [2, 0, 0] MSE=0.788
+
+    response_col = arima_model_imp(df_norm_col, 0.8, [7, 15], [0, 0, 2], [0, 0, 2], [6, 1, 2])
+    response_med = arima_model_imp(df_norm_medellin, 0.8, [7, 15], [4, 1, 2], [2, 1, 2], [0, 0, 2])
+    response_bog = arima_model_imp(df_norm_bogota, 0.8, [7, 15], [0, 0, 2], [0, 0, 2], [6, 1, 2])
+    response_cal = arima_model_imp(df_norm_cali, 0.8, [7, 15], [6, 1, 0], [2, 1, 4], [6, 1, 0])
+    response_car = arima_model_imp(df_norm_cartagena, 0.8, [7, 15], [4, 0, 3], [2, 0, 0], [2, 0, 0])
+    response_bar = arima_model_imp(df_norm_barranquilla, 0.8, [7, 15], [4, 0, 0], [2, 0, 0], [2, 0, 0])
+
     # %%
 
 if __name__ == '__main__':
     main()
+
+   # xanterior * (1 + pron_actual)
 
 
 
